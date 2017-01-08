842893157cf47e93feef7cbb9ee2932561ca82e1
pure-js <evolution.is.already.underway@gmail.com>
2017-01-08 20:50:16 +0300

    get pay-off date

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,5 +1,7 @@
-function payOffDate(startDate, term) {
-  return 34;
+function payOffYear(current, term) {
+  current = Number(current);
+  term = Number(term);
+  return current + term;
 }
 
 function setCurrentDate() {
@@ -7,4 +9,19 @@ function setCurrentDate() {
   document.getElementById('start-day').value = currentDate.getDate();
   document.getElementById('start-month').selectedIndex = currentDate.getMonth();
   document.getElementById('start-year').value = currentDate.getFullYear();
+}
+
+function calc() {
+  let term = Number(document.getElementById('loan-term').value);
+
+  let date = document.getElementById('start-day').value;
+  let fullDate = new Date();
+  let locale = 'en-us';
+  // let locale = 'ru-ru';
+  let monthLong = fullDate.toLocaleString(locale, { month: 'long' });
+  let month = document.getElementById('start-month').selectedIndex;
+  let currentYear = document.getElementById('start-year').value;
+  let year = payOffYear(currentYear, term);
+
+  document.getElementById('pay-off-date').innerHTML = date + ' ' + monthLong + ' ' + year;
 }
