5dfaa32fb6b6db33ebc91f94973780aa4a68b685
pure-js <evolution.is.already.underway@gmail.com>
2017-01-08 17:09:06 +0300

    gulp is working

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp'),
+  config = require('./gulp/config'),
+  paths = config.paths,
+  // build = require('./gulp/tasks/build'),
+  dev = require('./gulp/tasks/dev'),
+  plugins = require('gulp-load-plugins')(),
+  del = require('del');
+
+const clean = () => del([ paths.build, paths.dev, '.publish' ]);
+exports.clean = clean;
+
+gulp.task('deploy', () =>
+  gulp.src(paths.build + '**/*')
+    .pipe(plugins.ghPages())
+);
+
+// gulp.task('build', build);
+gulp.task('dev', dev);
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', dev);
